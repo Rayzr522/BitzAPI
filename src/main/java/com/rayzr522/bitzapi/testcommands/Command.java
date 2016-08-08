3a0e56e6faf038c4aff9fe6dd9ddff2990ee7ae1
EthanZeigler <ethanzeigler@gmail.com>
2016-08-08 07:41:10 -0400

    Working on Command system

--- src/main/java/com/rayzr522/bitzapi/testcommands/Command.java
+++ src/main/java/com/rayzr522/bitzapi/testcommands/Command.java
@@ -1,6 +1,8 @@
 package com.rayzr522.bitzapi.testcommands;
 
+import com.rayzr522.bitzapi.utils.data.ArrayUtils;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,17 +11,39 @@ import java.util.Map;
 /**
  * Created by Ethan on 8/6/16.
  */
-public class Command {
+public class Command implements CommandExecutor {
+    private String name;
     private Map<String, String> msgs;
     private Map<String, Command> subs;
     private PluginCommand command;
+    private CommandExecutor commandExecutor;
 
+    /**
+     * @param name
+     * @param cme
+     * @param plugin
+     */
     public Command(String name, CommandExecutor cme, JavaPlugin plugin) {
+        this.commandExecutor = cme;
         command = plugin.getCommand(name);
+        if (command != null) {
+            command.setExecutor(this);
+        }
+        this.name = name.toLowerCase();
+    }
+
+    public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args) { 
+        if (args.length > 1) {
+            Command subcommand;
+            if ((subcommand = (Command) subs.get(args[0].toLowerCase())) != null) {
+                subcommand.onCommand(sender, cmd, args[0].toLowerCase(), ArrayUtils.removeFirst(args));
+            }
+        }
+        return true;
     }
 
     public void addSubcommand(Command cmd) {
-
+        subs.put(cmd.getName(), cmd);
     }
 
     public void putMessage(String key, String msg) {
@@ -38,4 +62,11 @@ public class Command {
         return msgs.get(key);
     }
 
+    public org.bukkit.command.Command getBukkitCmd() {
+        return command;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
