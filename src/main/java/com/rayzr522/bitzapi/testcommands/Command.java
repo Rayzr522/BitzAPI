ffb486f78730b72f1ef6604d703efeb7f1f47ad2
Rayzr522 <rayzr522@users.noreply.github.com>
2016-08-09 09:21:04 -0400

    Modified Command system

--- src/main/java/com/rayzr522/bitzapi/testcommands/Command.java
+++ src/main/java/com/rayzr522/bitzapi/testcommands/Command.java
@@ -1,72 +1,223 @@
+
 package com.rayzr522.bitzapi.testcommands;
 
-import com.rayzr522.bitzapi.utils.data.ArrayUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Map;
+import com.rayzr522.bitzapi.utils.data.ArrayUtils;
 
 /**
  * Created by Ethan on 8/6/16.
  */
 public class Command implements CommandExecutor {
-    private String name;
-    private Map<String, String> msgs;
-    private Map<String, Command> subs;
-    private PluginCommand command;
-    private CommandExecutor commandExecutor;
 
-    /**
-     * @param name
-     * @param cme
-     * @param plugin
-     */
-    public Command(String name, CommandExecutor cme, JavaPlugin plugin) {
-        this.commandExecutor = cme;
-        command = plugin.getCommand(name);
-        if (command != null) {
-            command.setExecutor(this);
-        }
-        this.name = name.toLowerCase();
-    }
+	private String					name;
+	private Map<String, String>		msgs;
+	private Map<String, Command>	subs;
+	private PluginCommand			command;
+	private CommandExecutor			commandExecutor;
+	private String					regex;
+	private String					desc;
+	private String					usage;
 
-    public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args) { 
-        if (args.length > 1) {
-            Command subcommand;
-            if ((subcommand = (Command) subs.get(args[0].toLowerCase())) != null) {
-                subcommand.onCommand(sender, cmd, args[0].toLowerCase(), ArrayUtils.removeFirst(args));
-            }
-        }
-        return true;
-    }
+	/**
+	 * @param name
+	 * @param cme
+	 * @param plugin
+	 */
+	public Command(String name, CommandExecutor cme) {
+		this.commandExecutor = cme;
+		this.name = name.toLowerCase();
+		this.regex = name;
+	}
 
-    public void addSubcommand(Command cmd) {
-        subs.put(cmd.getName(), cmd);
-    }
+	public Command(String name, PluginCommand command, CommandExecutor cme) {
 
-    public void putMessage(String key, String msg) {
-        msgs.put(key, msg);
-    }
+		this(name, cme);
 
-    public Map<String, String> getMessageMap() {
-        return msgs;
-    }
+		this.command = command;
+		if (this.command.getExecutor() == null) {
+			this.command.setExecutor(this);
+		}
 
-    public void setMessages(Map<String, String> msgs) {
-        this.msgs = msgs;
-    }
+	}
 
-    public String getMsg(String key) {
-        return msgs.get(key);
-    }
+	public boolean onCommand(CommandSender sender, org.bukkit.command.Command cmd, String label, String[] args) {
 
-    public org.bukkit.command.Command getBukkitCmd() {
-        return command;
-    }
+		if (args.length > 1) {
 
-    public String getName() {
-        return name;
-    }
+			List<Command> matches = getMatches(args[0]);
+
+			if (matches.size() > 0) {
+
+				if (matches.size() > 1) {
+					// TODO: What happens when there's multiple commands that
+					// fit the bill?
+				}
+
+				return matches.get(0).onCommand(sender, cmd, args[0].toLowerCase(), ArrayUtils.removeFirst(args));
+
+			}
+
+		}
+
+		commandExecutor.onCommand(sender, cmd, label, args);
+
+		return true;
+	}
+
+	public List<Command> getMatches(String param) {
+
+		List<Command> matches = new ArrayList<Command>();
+
+		for (Command sub : subs.values()) {
+			if (param.matches(sub.getRegex())) {
+				matches.add(sub);
+			}
+		}
+
+		return matches;
+
+	}
+
+	public void addSubcommand(Command cmd) {
+		if (cmd.getPluginCommand() != command) {
+			cmd.setPluginCommand(command);
+		}
+		subs.put(cmd.getName(), cmd);
+	}
+
+	public Command addSubcommand(String name, CommandExecutor cme) {
+
+		Command cmd = new Command(name, command, cme);
+		addSubcommand(cmd);
+		return cmd;
+
+	}
+
+	public void setPluginCommand(PluginCommand command) {
+		this.command = command;
+	}
+
+	public PluginCommand getPluginCommand() {
+		return command;
+	}
+
+	public void putMessage(String key, String msg) {
+		msgs.put(keyFormat(key), msg);
+	}
+
+	public Map<String, String> getMessageMap() {
+		return msgs;
+	}
+
+	public void setMessages(Map<String, String> msgs) {
+		this.msgs = msgs;
+	}
+
+	public String getMsg(String key) {
+		return msgs.get(keyFormat(key));
+	}
+
+	/**
+	 * 
+	 * @param text
+	 * @return the text formatted in a consistent way
+	 */
+	public String keyFormat(String text) {
+
+		return text.toUpperCase().replaceAll(" ", "_");
+
+	}
+
+	/**
+	 * 
+	 * @return the Bukkit command that this is associated with. If this is a
+	 *         subcommand it will be the same as the root's command
+	 */
+	public org.bukkit.command.Command getBukkitCmd() {
+		return command;
+	}
+
+	public Command executorFor(PluginCommand command) {
+		this.command = command;
+		command.setExecutor(this);
+		return this;
+	}
+
+	/**
+	 * 
+	 * @return this command's name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * This doesn't do anything for the root command
+	 * 
+	 * @param regex
+	 * @return this command, useful for one-liners
+	 */
+	public Command setRegex(String regex) {
+
+		this.regex = regex;
+		return this;
+
+	}
+
+	/**
+	 * 
+	 * @return this command's regex. If none has been specified it defaults to
+	 *         the name of the command
+	 */
+	public String getRegex() {
+		return regex;
+	}
+
+	/**
+	 * Set the usage of this command
+	 * 
+	 * @param usage
+	 * @return this command, useful for one-liners
+	 */
+	public Command setUsage(String usage) {
+		this.usage = usage;
+		return this;
+	}
+
+	/**
+	 * Get the command usage
+	 * 
+	 * @return this command's usage, or null if none has been specified
+	 */
+	public String getUsage() {
+		return usage;
+	}
+
+	/**
+	 * Set the description of this command
+	 * 
+	 * @param desc
+	 * @return this command, useful for one-liners
+	 */
+	public Command setDesc(String desc) {
+		this.desc = desc;
+		return this;
+	}
+
+	/**
+	 * Get the description
+	 * 
+	 * @return this command's description, or null if none has been specified
+	 */
+	public String getDesc() {
+		return desc;
+	}
+
 }
