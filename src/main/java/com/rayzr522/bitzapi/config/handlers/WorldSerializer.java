88ea5d1e3b0bc4ed11a396ebdce5c30d5af7a55a
Rayzr522 <rayzr522@users.noreply.github.com>
2016-08-25 09:30:04 -0400

    Added WorldSerializer

---
+++ src/main/java/com/rayzr522/bitzapi/config/handlers/WorldSerializer.java
@@ -0,0 +1,36 @@
+
+package com.rayzr522.bitzapi.config.handlers;
+
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import com.rayzr522.bitzapi.config.data.ISerializationHandler;
+import com.rayzr522.bitzapi.utils.data.MapUtils;
+
+public class WorldSerializer implements ISerializationHandler<World> {
+
+	@Override
+	public Map<String, Object> serialize(World obj) {
+
+		Map<String, Object> map = MapUtils.empty();
+
+		map.put("uuid", obj.getUID().toString());
+
+		return map;
+	}
+
+	@Override
+	public World deserialize(Map<String, Object> map) {
+
+		UUID uuid = UUID.fromString((String) map.get("uuid"));
+
+		World world = Bukkit.getWorld(uuid);
+
+		return world;
+
+	}
+
+}
